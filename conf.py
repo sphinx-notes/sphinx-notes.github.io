ddcf950511026149229104ba6284b558e9fdcae5
Shengyu Zhang <i@silverrainz.me>
2021-03-13 20:43:46 +0800

    Bye, fujing.jiang

---
+++ conf.py
@@ -0,0 +1,82 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+from datetime import datetime
+# Import proj's meta info
+
+# -- Project information -----------------------------------------------------
+
+project = 'Sphinx Notes'
+author = 'Shengyu Zhang'
+copyright = '%s, %s' % (datetime.now().year, author)
+
+# The full version, including alpha/beta/rc tags
+version = release = 'N/A'
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinx.ext.githubpages',
+    'sphinx.ext.extlinks',
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# The document name of the “master” document, that is,
+# the document that contains the root toctree directive.
+# Default is 'index', we set it here for supporting Sphinx<2.0
+master_doc = 'index'
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# A boolean that decides whether codeauthor and sectionauthor directives
+# produce any output in the built files.
+show_authors = True
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'alabaster'
+
+html_theme_options = {
+    'nosidebar': True,
+}
+
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+html_css_files = ['center.css']
+
+# The URL which points to the root of the HTML documentation.
+# It is used to indicate the location of document like canonical_url
+html_baseurl = 'https://sphinx-notes.github.io/'
+
+html_logo = html_favicon = '_images/sphinx-notes.png'
+
+# If true, Sphinx will add “permalinks” for each heading and description environment.
+html_permalinks = False
+
+extlinks = {
+    'sphinxnotes': (html_baseurl + '%s', 'sphinxnotes-'),
+}
